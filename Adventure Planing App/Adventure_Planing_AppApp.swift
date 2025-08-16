5a0a1707ecaf72315694a2b508bb62c4346d6e25
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-16 19:12:11 +0530

    Initial Commit

---
+++ Adventure Planing App/Adventure_Planing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_Planing_AppApp.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Adventure_Planing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adventure Planing AppTests/Adventure_Planing_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_Planing_AppTests.swift
+//  Adventure Planing AppTests
+//
+//  Created by Tharangani Hagoda Arachchi on 16/08/2025.
+//
+
+import Testing
+@testable import Adventure_Planing_App
+
+struct Adventure_Planing_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
