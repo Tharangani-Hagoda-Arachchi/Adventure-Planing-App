1778f82dc59a55b22b77d7a60a835494d0d13a4e
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-18 09:54:20 +0530

    error handeling - register form

---
+++ Views/CommonViews/ErrorTextView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorTextView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/08/2025.
+//
+
+import SwiftUI
+
+struct ErrorTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorTextView()
+}
