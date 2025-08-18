ec689c7f25d33d66acba27bfa71f386fe55926b7
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-18 10:23:45 +0530

    Design login screen

--- ViewModels/LoginViewModel.swift
+++ ViewModels/LoginViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import SwiftUI
+
+class LoginViewModel : ObservableObject{
+    
+    @Published var email = ""
+    @Published var password = ""
+
+    //error masseges
+    @Published var errorEmail : String?
+    @Published var errorPassword : String?
+
+    //validation status
+    @Published var isValid = false
+
+    
+    // validation check function
+    func validateCreateAccount(){
+        
+        //assign errors to nill
+        errorEmail = nil
+        errorPassword = nil
+
+        if email.isEmpty{
+            errorEmail = "Enter Your Email"
+        }
+        
+        if password.isEmpty{
+            errorPassword = "Enter Your Password"
+        }
+        
+        if errorEmail == nil && errorPassword == nil {
+            isValid = true
+        }
+    }
+    
+}
+
+
+
+
+
--- Views/LoginView.swift
+++ Views/LoginView.swift
@@ -8,9 +8,98 @@
 import SwiftUI
 
 struct LoginView: View {
+    @StateObject private var loginViewModel = LoginViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationStack{
+            ZStack(alignment: .bottom){
+                
+                // backgroun image section
+                Rectangle()
+                    .fill(ImagePaint(image: Image("Login Background"), scale: 0.65))
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .ignoresSafeArea(.all)
+                
+                // BOTTEM FREAME SECTION
+                VStack(spacing: 12){
+                    
+                    //heading and sub heading section
+                    VStack{
+                        Text("Login")
+                            .font(Font.TitleText)
+                        Text("Let's Get Started")
+                            .font(Font.SubTitleText)
+                        Text("Log Your Account")
+                            .font(Font.SubTitleSmallText)
+                        
+                    }
+                    .padding()
+                    .padding(.horizontal, 16)
+                    
+                    //create form using custom text field
+                    VStack(spacing: 12,){
+                        
+                        CustomTextFieldView(
+                            icon: "envelope.fill", placeHolder: "Email", text: $loginViewModel.email
+                        )
+                        ErrorTextView(error: loginViewModel.errorEmail)
+                        
+                        CustomTextFieldView(
+                            icon: "lock.fill", placeHolder: "Password", isSecure: true, text: $loginViewModel.password
+                        )
+                        ErrorTextView(error: loginViewModel.errorPassword)
+                        
+                        //login button
+                        VStack{
+                            CustomPrimaryButtonView(title: "Login"){
+                                loginViewModel.validateCreateAccount()
+                                if loginViewModel.isValid{
+                                    print("tap Login")
+                                    //logic with backend
+                                }
+                            }
+                            .padding(.top, 20)
+                            .padding(.bottom, 10)
+                            
+                            NavigationLink(destination: PasswodRecoveryIView()){
+                                Text("Forget Password?")
+                                    .font(Font.primarysSemiboldText)
+                                    .foregroundColor(Color.AppSecondary)
+                                
+                            }
+                            
+                            HStack{
+                                Text("Create new account")
+                                    .font(Font.primarysSemiboldText)
+                                NavigationLink(destination: CreateAccountView()){
+                                    Text("Register")
+                                        .font(Font.primarysSemiboldText)
+                                        .foregroundColor(Color.AppSecondary)
+                                    
+                                }
+                                
+                            }
+                            
+                            
+                        }
+                        
+                    }
+                    .padding(.bottom, 70)
+                    .padding(.horizontal, 16)
+                }
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 5)
+            }
+            
+            
+        }
+        .navigationBarHidden(true)
+            
+        }
+        
+    
 }
 
 #Preview {
