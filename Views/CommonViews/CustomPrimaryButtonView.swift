860a96bcd19eff6a3fa2e9915e6f3e8b42730535
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-18 08:53:07 +0530

    crezte CustomPrimaryButtonView

---
+++ Views/CommonViews/CustomPrimaryButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPrimaryButtonView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/08/2025.
+//
+
+import SwiftUI
+
+struct CustomPrimaryButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPrimaryButtonView()
+}
