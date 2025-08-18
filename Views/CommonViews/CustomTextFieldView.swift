33883bf6ea8ed562d7594145220c7c06c4f6a4a5
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-18 07:27:55 +0530

    design custom text field

---
+++ Views/CommonViews/CustomTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextFieldView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/08/2025.
+//
+
+import SwiftUI
+
+struct CustomTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextFieldView()
+}
