25fec38b05e34ff650903a58cf1d944a9f1b0e8b
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-20 13:49:53 +0530

    create guide view and guide view mode

---
+++ ViewModels/GuideViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GuideViewModel.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 20/08/2025.
+//
+
+import Foundation
---
+++ Views/GuideView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 20/08/2025.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideView()
+}
