9dbdcb18692415901e753c2af6632b25ebb9502f
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-20 22:40:13 +0530

    create guide views

---
+++ Views/CommonViews/GuideDetailCardView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideDetailCardView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 20/08/2025.
+//
+
+import SwiftUI
+
+struct GuideDetailCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideDetailCardView()
+}
---
+++ Views/GuideDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideDetailView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 20/08/2025.
+//
+
+import SwiftUI
+
+struct GuideDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideDetailView()
+}
