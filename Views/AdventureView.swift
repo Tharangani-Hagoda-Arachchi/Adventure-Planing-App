6177b1f887cadecf882efed127157e98bd88fe4b
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-09-17 03:07:06 +0530

    create adventure view

---
+++ Views/AdventureView.swift
@@ -0,0 +1,18 @@
+//
+//  AdventureView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 16/09/2025.
+//
+
+import SwiftUI
+
+struct AdventureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdventureView()
+}
---
+++ Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 16/09/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
