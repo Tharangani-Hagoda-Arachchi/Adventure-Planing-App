8c03d8733320f53bbc67ec59621bf98a56f3a8e4
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-09-19 01:39:36 +0530

    create pament process

---
+++ Views/CommonViews/TravelDatePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  TravelDatePickerView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/09/2025.
+//
+
+import SwiftUI
+
+struct TravelDatePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TravelDatePickerView()
+}
---
+++ Views/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 19/09/2025.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
