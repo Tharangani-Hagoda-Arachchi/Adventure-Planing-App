a34ce89d22d13e6d56c92af5d83b45a054ef9980
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-09-18 18:41:09 +0530

    create token manger

---
+++ Services/TokenManager.swift
@@ -0,0 +1,8 @@
+//
+//  TokenManager.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/09/2025.
+//
+
+import Foundation
---
+++ Views/ContatDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ContatDetailView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 18/09/2025.
+//
+
+import SwiftUI
+
+struct ContatDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContatDetailView()
+}
