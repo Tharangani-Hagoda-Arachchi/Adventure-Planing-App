745c05dba496099b4bb7e30fa3f6a8a78279083e
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-09-25 18:30:27 +0530

    create serch views

---
+++ Views/Search/SearchResultView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultView.swift
+//  Adventure Planing App
+//
+//  Created by Tharangani Hagoda Arachchi on 25/09/2025.
+//
+
+import SwiftUI
+
+struct SearchResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultView()
+}
